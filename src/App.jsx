05f1f2ceeb18328d5a3d315ae3d794b63ead0839
Anunay Argha <anunayargha@gmail.com>
2024-03-26 21:14:28 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function App() {
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [isNext, setIsNext] = useState(false);
+
+	const handleClick = function (i) {
+		if (squares[i]) return;
+		const nextSquare = squares.slice();
+		nextSquare[i] = isNext ? "images/circle.png" : "images/cross.png";
+		setSquares(nextSquare);
+		setIsNext(!isNext);
+	};
+
+	return (
+		<div className="bg-[#111B22] min-h-screen grid place-items-center">
+			<h1 className="text-6xl text-white font-bold">
+				Tic Tac Toe Game in <span className="text-[#56FFC8]">React</span>
+			</h1>
+			<div className="grid aspect-square grid-cols-3 w-max gap-2">
+				{squares.map((value, i) => (
+					<Square
+						value={value}
+						key={i}
+						onSquareClick={handleClick.bind(squares, i)}
+					/>
+				))}
+			</div>
+			<button className="text-[#56FFC8] text-xl font-semibold bg-[#223541] px-10 py-3 rounded-full">
+				Reset
+			</button>
+		</div>
+	);
+}
+
+function Square({ value, onSquareClick }) {
+	return (
+		<button
+			className="bg-[#223541] aspect-square w-40 rounded-md grid place-content-center"
+			onClick={onSquareClick}
+		>
+			{/* {value} */}
+			<img src={value} />
+		</button>
+	);
+}
